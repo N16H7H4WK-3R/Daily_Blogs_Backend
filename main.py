c370dfdb3e7b44f2fd5b7790a9aa3d507c6ddb64
N16H7H4WK-3R <aryan014kumar@gmail.com>
2024-07-06 19:08:21 +0530

    TimeZone Bug Fixes

--- main.py
+++ main.py
@@ -2,8 +2,21 @@ from fastapi import FastAPI
 from blog import  models
 from blog.database import engine
 from blog.routers import blog, user, authentication
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 models.Base.metadata.create_all(engine)
 
